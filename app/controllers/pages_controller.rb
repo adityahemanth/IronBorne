1023b3b9ed0d2fb09206b52202b1249e31017e2a
adityahemanth <hemanth.aditya93@gmail.com>
2017-06-22 02:01:16 +0000

    Add static pages controller.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+# Pages controller for static pages.
+class PagesController < ApplicationController
+  # GET /home
+  def home
+  end
+
+  # GET /about
+  def about
+  end
+
+  # GET /contact
+  def contact
+  end
+end
